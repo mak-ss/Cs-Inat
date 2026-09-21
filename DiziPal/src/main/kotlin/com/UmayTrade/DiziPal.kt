4a047169cd9dc3b9f52085977b32e4f302679e97
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-22 00:01:34 +0300

    Update mainUrl to new DiziPal domain

--- DiziPal/src/main/kotlin/com/UmayTrade/DiziPal.kt
+++ DiziPal/src/main/kotlin/com/UmayTrade/DiziPal.kt
@@ -8,7 +8,7 @@ import com.lagradost.cloudstream3.utils.*
 import org.jsoup.nodes.Element
 
 class DiziPalOriginal : MainAPI() {
-    override var mainUrl              = "https://dizipal2131.com"
+    override var mainUrl              = "https://dizipal2133.com"
     override var name                 = "DiziPal"
     override val hasMainPage          = true
     override var lang                 = "tr"
