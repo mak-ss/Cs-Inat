d84006ba871c07684b387f0683583d330aa8f189
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-22 13:20:48 +0300

    Fix formatting of DiziBalPlugin class definition

--- DiziBal/src/main/kotlin/com/Blockades/DiziBalPlugin.kt
+++ DiziBal/src/main/kotlin/com/Blockades/DiziBalPlugin.kt
@@ -5,8 +5,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
-class DiziBalPlugin: Plugin() {
-
+class DiziBalPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(DiziBal())
     }
