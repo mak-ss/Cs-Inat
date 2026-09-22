8a686b6a04bb43e76ab3904c8dae59d93a24e87a
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-22 13:08:52 +0300

    Change status value from 1 to 0

--- DiziBol/build.gradle.kts
+++ DiziBol/build.gradle.kts
@@ -13,7 +13,7 @@ cloudstream {
      * 2: Slow
      * 3: Beta only
     **/
-    status  = 1 // will be 3 if unspecified
+    status  = 0 // will be 3 if unspecified
     tvTypes = listOf("Movie", "TvSeries", "Anime")
     iconUrl = "https://www.google.com/s2/favicons?domain=dizibol.org&sz=128"
 }
