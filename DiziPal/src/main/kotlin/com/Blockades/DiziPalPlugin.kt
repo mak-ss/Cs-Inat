4bdf8c5a5c43cdc16933209f0d666ec683679c2d
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-23 23:54:34 +0300

    Refactor DiziPalPlugin to extend BasePlugin

--- DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
@@ -1,12 +1,11 @@
 package com.Blockades
 
+import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
-class DiziPalPlugin : Plugin() {
-    override fun load(context: Context) {
-        registerMainAPI(DiziPal())
+class DizipalPlugin : BasePlugin() {
+    override fun load() {
+        registerMainAPI(Dizipal())
     }
 }
