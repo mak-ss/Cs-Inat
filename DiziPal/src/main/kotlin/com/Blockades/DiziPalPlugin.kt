291874ca3d924a960987e0b7cdedfc281a02e2ae
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-24 02:08:26 +0300

    Replace Dizipal with DizipalProvider in load method

--- DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
@@ -2,11 +2,10 @@ package com.Blockades
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
-import com.lagradost.cloudstream3.providers.DizipalProvider // Import the provider
 
 @CloudstreamPlugin
 class DiziPalPlugin : BasePlugin() {
     override fun load() {
-        registerMainAPI(Dizipal())
+        registerMainAPI(DizipalProvider())
     }
 }
