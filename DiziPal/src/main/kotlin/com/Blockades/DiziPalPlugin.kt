5e9a4c728c8c7c73b6a3a5cc7579b7085433cdaa
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-24 02:36:24 +0300

    Update DiziPalPlugin.kt

--- DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
@@ -3,12 +3,11 @@ package com.Blockades
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
-import com.Blockades.DiziPal // DiziPal.kt dosyasındaki paketi import ediyoruz
 
 @CloudstreamPlugin
 class DiziPalPlugin: Plugin() {
     override fun load(context: Context) {
-        // Ana sağlayıcıyı Cloudstream'e kaydediyoruz
+        // İki dosya da aynı pakette olduğu için doğrudan erişebilir
         registerMainAPI(DiziPal())
     }
 }
