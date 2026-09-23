52baf6bbfd8490509feaa428a49591c1a2602c58
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-24 02:05:21 +0300

    Update DiziPalPlugin.kt

--- DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
@@ -2,10 +2,11 @@ package com.Blockades
 
 import com.lagradost.cloudstream3.plugins.BasePlugin
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.providers.DizipalProvider // Import the provider
 
 @CloudstreamPlugin
 class DiziPalPlugin : BasePlugin() {
     override fun load() {
-        registerMainAPI(DiziPalOrijinal())
+        registerMainAPI(Dizipal())
     }
 }
