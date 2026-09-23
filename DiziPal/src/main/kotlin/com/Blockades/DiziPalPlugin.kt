0fb3a0b79aa62ce4cac9f1085e274f46e2516bb2
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-23 11:13:12 +0300

    Update DiziPalPlugin.kt

--- DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
@@ -7,6 +7,7 @@ import android.content.Context
 @CloudstreamPlugin
 class DiziPalPlugin: Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(DiziPal())
+        // Class ismi DiziPalOriginal olarak kaydediliyor
+        registerMainAPI(DiziPalOriginal())
     }
 }
