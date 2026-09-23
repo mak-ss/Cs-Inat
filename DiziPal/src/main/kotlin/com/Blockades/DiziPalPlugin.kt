518aebca5af82223bcfdea3450b1e6e59f90dd9d
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-24 02:00:06 +0300

    Replace DiziPal with DiziPalOrijinal in load method

--- DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
@@ -6,6 +6,6 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 @CloudstreamPlugin
 class DiziPalPlugin : BasePlugin() {
     override fun load() {
-        registerMainAPI(DiziPal())
+        registerMainAPI(DiziPalOrijinal())
     }
 }
