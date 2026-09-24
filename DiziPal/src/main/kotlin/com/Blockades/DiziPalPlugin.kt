678bb9d4ae96db30f381ab80e5ee4df3080f7efd
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-25 01:18:28 +0300

    Refactor DiziPalPlugin class formatting

--- DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class DiziPalPlugin : Plugin() {
+class DiziPalPlugin: Plugin() {
     override fun load(context: Context) {
         registerMainAPI(DiziPalOriginal())
     }
