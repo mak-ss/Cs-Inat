0d8242a090a97365da8a5807fc5b686350531084
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-25 00:39:46 +0300

    Refactor DiziPalPlugin to use DiziPalOriginal

--- DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
+++ DiziPal/src/main/kotlin/com/Blockades/DiziPalPlugin.kt
@@ -5,9 +5,8 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class DiziPalPlugin: Plugin() {
+class DiziPalPlugin : Plugin() {
     override fun load(context: Context) {
-        // İki dosya da aynı pakette olduğu için doğrudan erişebilir
-        registerMainAPI(DiziPal())
+        registerMainAPI(DiziPalOriginal())
     }
 }
