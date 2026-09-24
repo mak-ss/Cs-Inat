c5a41c0eae0eaaf7c39713a31be82aebbd04a7f6
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-24 11:07:48 +0300

    Update DiziPal.kt

--- DiziPal/src/main/kotlin/com/Blockades/DiziPal.kt
+++ DiziPal/src/main/kotlin/com/Blockades/DiziPal.kt
@@ -1,6 +1,6 @@
 // ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
 
-package com.keyiflerolsun
+package com.Blockades
 
 import android.util.Log
 import com.lagradost.cloudstream3.*
