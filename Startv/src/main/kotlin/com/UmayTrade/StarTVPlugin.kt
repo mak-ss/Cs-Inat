db11e1d3be5e9796cfa83df5dd66e7f66c8c3bf3
mak-ss <mehmet.kocatepe@gmail.com>
2026-09-26 04:17:47 +0300

    Fix class name case in StarTVPlugin

--- Startv/src/main/kotlin/com/UmayTrade/StarTVPlugin.kt
+++ Startv/src/main/kotlin/com/UmayTrade/StarTVPlugin.kt
@@ -7,6 +7,6 @@ import android.content.Context
 @CloudstreamPlugin
 class StarTvPlugin : Plugin() {
     override fun load(context: Context) {
-        registerMainAPI(StarTV())
+        registerMainAPI(StarTv())
     }
 }
